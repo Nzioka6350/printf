e9218e5da1a9c31fd05dc8e1d0bec16854183072
iamwairimu <kmnleah@mail.com>
2023-07-25 03:08:33 -0700

    rectified

--- handleprint.c
+++ handleprint.c
@@ -1,18 +1,19 @@
 #include "main.h"
 /**
- * @handle_print - print argument based on its type
- * @fmt: string to print arguments in
- * @list: list of arguments
- * @ind: ind
- * @buffer: buffer array handle print
- * @flags: calculates active flags
- * @width: get width
- * @precision: precision specification
- * @size: size specifier
- * Return: 1 or 2
+ * handle_print - Prints an argument based on its type
+ * @fmt: Formatted string in which to print the arguments.
+ * @list: List of arguments to be printed.
+ * @ind: index to track current position in 'fmt' string
+ * @buffer: Buffer array to handle print.
+ * @flags: specifies active flags for formatting
+ * @width: width specification for formatting
+ * @precision: Precision specification for formatting
+ * @size: Size specifier for formatting
+ * Return: number of characters printed on success,
+ * -1 on failure
  */
-int handle_print(const char *fmt, int *ind, va_list list, char buffer[]),
-int flags, int width, int precision, int size)
+int handle_print(const char *fmt, int *ind, va_list list, char buffer[],
+	int flags, int width, int precision, int size)
 {
 	int i, unknow_len = 0, printed_chars = -1;
 	fmt_t fmt_types[] = {
@@ -22,22 +23,23 @@ int flags, int width, int precision, int size)
 		{'X', print_hexa_upper}, {'p', print_pointer}, {'S', print_non_printable},
 		{'r', print_reverse}, {'R', print_rot13string}, {'\0', NULL}
 	};
-	for (i = 0; fmt_types[i].fmt != '\0; i++)
+	for (i = 0; fmt_types[i].fmt != '\0'; i++)
 		if (fmt[*ind] == fmt_types[i].fmt)
 			return (fmt_types[i].fn(list, buffer, flags, width, precision, size));
+
 	if (fmt_types[i].fmt == '\0')
 	{
 		if (fmt[*ind] == '\0')
 			return (-1);
 		unknow_len += write(1, "%%", 1);
-		if (fmt[*ind - 1] == '')
-			unknow_len +=write(1, "", 1);
+		if (fmt[*ind - 1] == ' ')
+			unknow_len += write(1, " ", 1);
 		else if (width)
 		{
 			--(*ind);
-			while (fmt[*ind] != '' && fmt[*ind] != '%')
+			while (fmt[*ind] != ' ' && fmt[*ind] != '%')
 				--(*ind);
-			if (fmt[*ind] == '')
+			if (fmt[*ind] == ' ')
 				--(*ind);
 			return (1);
 		}
