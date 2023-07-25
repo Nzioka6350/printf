f77418715130d5b48e6f91df72311d1fcb7c8cfe
iamwairimu <kmnleah@mail.com>
2023-07-25 01:38:23 -0700

    size

---
+++ size.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * get_size - size to cast arguments
+ * @format: string to print arguments
+ * @i: arguments to print
+ * Return: precision
+ */
+int get_size(const char *format, int *i)
+{
+	int curr_i = *i + 1;
+	int size = 0;
+
+	if (format[curr_i] == 'l')
+		size = s_long;
+	else if (format[curr_i] == 'h')
+		size = s_short;
+	if (size == 0)
+		*i = curr_i - 1;
+	else
+		*i = curr_i;
+	return (size);
+}
